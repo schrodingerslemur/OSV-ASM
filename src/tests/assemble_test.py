6a3dedee56d73aab14573b282da98509e48fcb8d
Brendan Hendrata <bhendrata@cmu.edu>
2025-12-27 00:37:42 -0500

    added missing operation error

--- src/tests/assemble_test.py
+++ src/tests/assemble_test.py
@@ -1,6 +1,7 @@
 import sys
 
-from src.assemble import parse_op
+from src.assemble import parse_op, handle_address_and_label
+from src.errors import MissingOperationError
 
 def test_parse_op_valid():
     line = "ADD R1, R2, R3"
@@ -13,3 +14,10 @@ def test_parse_op_valid():
     print(non_op)
     assert op == "SUB"
     assert non_op == "R4, R5, R6"
+
+def test_parse_op_invalid():
+    line = "   "
+    try:
+        parse_op(line)
+    except MissingOperationError as e:
+        assert isinstance(e, MissingOperationError)
