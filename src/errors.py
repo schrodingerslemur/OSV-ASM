c496b49583213697a524244d377f7588a8ba7139
Brendan Hendrata <bhendrata@cmu.edu>
2025-12-27 00:29:51 -0500

    added invalid and missing operation handling

--- src/errors.py
+++ src/errors.py
@@ -2,6 +2,10 @@ class AssemblyError(Exception):
     """Custom exception for assembly errors."""
     pass
 
+class MissingOperationError(AssemblyError):
+    """Exception raised for missing operations."""
+    pass
+
 class InvalidOperationError(AssemblyError):
     """Exception raised for invalid operations."""
     pass
