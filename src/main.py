b69f9a768092ce2bb9a7549c17f13690b310883c
Brendan Hendrata <bhendrata@cmu.edu>
2025-12-31 02:31:46 -0500

    added assertion error

--- src/main.py
+++ src/main.py
@@ -56,8 +56,10 @@ def main(
         logger.info(f"Assembly complete. Output written to {output_file_name}")
     except AssemblyError as ae:
         logger.error(f"Assembly error: {ae}")
-    # except Exception as e:
-    #     logger.error(f"An unexpected error occurred during assembly: {e}")
+    except AssertionError as ase:
+        logger.error(f"Assertion error during assembly: {ase}")
+    except Exception as e:
+        logger.error(f"An unexpected error occurred during assembly: {e}")
     
     return
 
